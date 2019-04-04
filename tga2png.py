12f7d41b9f2ffaf7a60a67bb3c5d8973513153f2
miaofengliang <miaofengliang@ztgame.com>
2019-04-04 17:28:15 +0800

    增加多线程处理

--- tga2png.py
+++ tga2png.py
@@ -2,8 +2,53 @@
 # -*- coding:utf-8 -*-
 
 import os
-from PIL import Image
+import sys
+import time
 import optparse
+import threading
+from PIL import Image
+
+
+def thread_run(file_list, max_thread_count=10):
+    start = time.time()
+    obj = {
+        "threadLock": threading.Lock(),
+        "threads": [],
+        "index": 0,
+    }
+    
+    file_count = len(file_list)
+
+    def run():
+        while obj["index"] < file_count:
+            fileRealPath, fileToPath = file_list[obj["index"]]
+            idx = obj["index"]
+
+            obj["threadLock"].acquire()
+            obj["index"] += 1
+            obj["threadLock"].release()
+            
+            if os.path.exists(fileToPath):
+                print("%s is exist pass" % fileToPath)
+                continue
+            
+            print("%s/%s  %s -> %s" % (idx, file_count, fileRealPath, fileToPath))
+            fileToFolder = os.path.dirname(fileToPath)
+            if not os.path.exists(fileToFolder):
+                os.makedirs(fileToFolder)
+            
+            Image.open(fileRealPath).save(fileToPath)
+
+    for _ in xrange(max_thread_count):
+        t = threading.Thread(target=run)
+        t.start()
+        obj["threads"].append(t)
+
+    for t in obj["threads"]:
+        t.join()
+
+    end = time.time()
+    print "thread_run, duration %s" % (end - start)
 
 def convert(folderFrom, folderTo):
     folderFrom = os.path.realpath(folderFrom)
@@ -11,22 +56,18 @@ def convert(folderFrom, folderTo):
     
     print("convert from %s to %s" % (folderFrom, folderTo))
     
+    # [(fileFrom, fileTo)]
+    file_list = []
+    
     for root, dirs, files in os.walk(folderFrom):
         for filePath in files:
             if filePath.endswith(".tga"):
                 fileRealPath = os.path.realpath(root + "/" + filePath)
                 fileToPath = os.path.realpath(folderTo + "/" + root[len(folderFrom):] + "/" + filePath[:-len(".tga")] + ".png")
                 
-                if os.path.exists(fileToPath):
-                    print("%s is exist pass" % fileToPath)
-                    continue
-                
-                print("%s -> %s" % (fileRealPath, fileToPath))
-                fileToFolder = os.path.dirname(fileToPath)
-                if not os.path.exists(fileToFolder):
-                    os.makedirs(fileToFolder)
-                
-                Image.open(fileRealPath).save(fileToPath)
+                file_list.append((fileRealPath, fileToPath))
+    
+    thread_run(file_list, 30)
                
     print("convert end.")
 
